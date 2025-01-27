7fd503eec12c3fd397ad7c7b706b3dd3d8c60d44
Mustafa KURAL <mustafakural@outlook.com>
2025-01-27 03:07:59 +0300

    ilk

---
+++ u1_merhaba/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BTK AKADEMİ!");//metin
+Console.WriteLine("İzmir 2025");
+Console.WriteLine(2025);//tamsayı
+Console.WriteLine(4.5);//ondalıklı sayı
+Console.WriteLine(true);//mantıksal
+Console.WriteLine('A');//karakter
+
