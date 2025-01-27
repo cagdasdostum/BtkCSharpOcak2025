deb7bc70f8dda7779790b5d33448c48d207ef3bb
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 01:51:43 +0300

    askerlik

--- u13_askerlik_durumu/Program.cs
+++ u13_askerlik_durumu/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Yaşınızı girin:");
+int yas = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Cinsiyetinizi girin [E/K]:");
+char cinsiyet = Convert.ToChar(Console.ReadLine());
+/*
+if(cinsiyet == 'E' && yas >= 20)
+{
+    Console.WriteLine("ASKERE GİDEBİLİR.........");
+}
+else
+{
+    Console.WriteLine("ASKERE GİDEMEZ.........");
+}
+*/
+
+if(cinsiyet=='K' || yas < 20)
+{
+    Console.WriteLine("ASKERE GİDEMEZ......");
+}
+else
+{
+    Console.WriteLine("ASKERE GİDEBİLİR......");
+}
+
+
