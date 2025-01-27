cb31f40c997e9f0364533760cd5b717fbb510b00
Mustafa KURAL <mustafakural@outlook.com>
2025-01-27 05:33:05 +0300

    string interpolation

--- u5_metin_birlestirme/Program.cs
+++ u5_metin_birlestirme/Program.cs
@@ -3,3 +3,7 @@
 int sonuc = a * b;//15
 
 Console.WriteLine(a + " * " + b + " = " + sonuc);
+Console.WriteLine(string.Format("{0} * {1} = {2}", a, b, sonuc));
+Console.WriteLine("{0} * {1} = {2}", a, b, sonuc);
+
+Console.WriteLine($"{a} * {b} = {sonuc}");//string interpolation
