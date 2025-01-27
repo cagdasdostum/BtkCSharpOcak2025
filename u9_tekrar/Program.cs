9e3e2aff4897c5a39e4b4860a04b6ca27c504c5d
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 00:36:26 +0300

    tekrar yapıldı

---
+++ u9_tekrar/Program.cs
@@ -0,0 +1,23 @@
+﻿//Değişken tanımlama ve veri türleri
+int sayi = 155;
+var a = 3.14;
+string metin = "35";
+
+//tip dönüşüm işlemleri
+int b = Convert.ToInt32(metin);//Convert
+byte c = (byte)sayi;//Casting
+
+//ekrana yazdırma
+Console.WriteLine(sayi);
+Console.WriteLine(c);
+//klavyeden okuma
+Console.WriteLine("Merhaba, Nasılsın?");
+string cevap = Console.ReadLine();
+Console.WriteLine("Klavyeden bir sayı girer misin?");
+int girilenSayi = Convert.ToInt32(Console.ReadLine());
+//değerleri birleştirme
+Console.WriteLine($"Cevabınız: {cevap} \nGirdiğiniz sayı: {girilenSayi}");
+//operatörler
+byte d = 4;//00000100 = 4  ====> 11111011 = 251
+
+Console.WriteLine((byte)~d);
