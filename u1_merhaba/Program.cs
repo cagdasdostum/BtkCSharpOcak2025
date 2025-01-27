b9fdb96fe034df73cb9687375abfcc1c48a391d5
Mustafa KURAL <mustafakural@outlook.com>
2025-01-27 03:17:28 +0300

    daD

--- u1_merhaba/Program.cs
+++ u1_merhaba/Program.cs
@@ -4,5 +4,5 @@ Console.WriteLine("İzmir 2025");
 Console.WriteLine(2025);//tamsayı
 Console.WriteLine(4.5);//ondalıklı sayı
 Console.WriteLine(true);//mantıksal
-Console.WriteLine('C');//karakter
+Console.WriteLine('D');//karakter
 
