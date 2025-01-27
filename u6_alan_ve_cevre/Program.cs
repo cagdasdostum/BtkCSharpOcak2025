5ead1f625f91c03bac6f985b124e11a19aa7170a
Mustafa KURAL <mustafakural@outlook.com>
2025-01-27 05:53:06 +0300

    alan ve cevre hesabı ve string interpolation ile çıktı

---
+++ u6_alan_ve_cevre/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Dörtgenin a kenarını gir:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Dörtgenin b kenarını gir:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+var alan = a * b;//sağdaki işlemin tipine göre değişken tipi belirler
+var cevre = (a + b) * 2;//parantezler işlem önceliği belirler
+
+// Örnek;
+//Kenarları 5 ve 3 olan bir dörtgenin alanı 15 'tir.
+//Kenarları 5 ve 3 olan bir dörtgenin çevresi 16 'tir.
+Console.WriteLine($"Kenarları {a} ve {b} olan bir dörtgenin alanı {alan} eder.");
+Console.WriteLine($"Kenarları {a} ve {b} olan bir dörtgenin çevresi {cevre} eder.");
