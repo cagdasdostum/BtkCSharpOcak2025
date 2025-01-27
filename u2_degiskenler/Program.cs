412c4520ab7eb67afcd56114ccb581214be2c145
Mustafa KURAL <mustafakural@outlook.com>
2025-01-27 03:57:03 +0300

    değişkenler uygulaması yapıldı

---
+++ u2_degiskenler/Program.cs
@@ -0,0 +1,14 @@
+﻿//değişkeni tanımlama ve ilk değer verme
+int plaka = 35;
+string sehir = "İzmir";
+double deger = 125.78;
+int a, b=4, c;
+
+a = 1;
+c = b;
+
+Console.WriteLine(plaka);
+Console.WriteLine(sehir);
+Console.WriteLine(deger);
+Console.WriteLine(a);
+
