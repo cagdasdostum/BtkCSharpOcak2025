39376734997e5b14bac4799ed89423aec5412da3
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 05:34:24 +0300

    üçgen çiz

---
+++ u20_ucgen_ciz/Program.cs
@@ -0,0 +1,18 @@
+﻿//kullanıcının girdiği yükseklik değerine göre
+//ekrana üçgeni * karakteri ile çizsin
+//     *                    genişlik=4
+//     **                   yükseklik=4
+//     ***
+//     ****
+
+int yuk = 4;
+
+for(int i = 0; i < yuk; i++)//bir defa başladımı tekrar başlamaz
+{
+    for(int j = 0; j < i + 1; j++)//her seferinde baştan başlar ve tamamlanır
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();//alt satıra geçer
+}
