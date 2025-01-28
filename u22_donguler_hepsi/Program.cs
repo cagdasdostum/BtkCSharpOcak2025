4cebe00086a4f116455213c824576fece32cf93d
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 06:12:40 +0300

    üç döngü bir arada

---
+++ u22_donguler_hepsi/Program.cs
@@ -0,0 +1,23 @@
+﻿//1 den 20'ye kadar olan tek sayıları ekrana yazdırsın
+Console.WriteLine("For döngüsü ile yapıldı");
+for (int i = 1; i<=20; i+=2)
+{
+    Console.Write($"{i}, ");
+}
+
+Console.WriteLine("\nWhile döngüsü ile yapıldı");
+int j = 1;
+while (j<=20)
+{
+    Console.Write($"{j}, ");
+    j+=2;
+}
+
+Console.WriteLine("\nDo-While döngüsü ile yapıldı");
+int k = 1;
+do
+{
+    Console.Write($"{k}, ");
+    k+=2;
+
+} while(k<=20);
