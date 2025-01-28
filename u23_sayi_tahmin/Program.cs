0470c18aa7ca7cb19e3766ba90e86b0f415adbed
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 01:24:17 +0300

    sayı tahmin oyununda tekrar oynamak için düzenleme

--- u23_sayi_tahmin/Program.cs
+++ u23_sayi_tahmin/Program.cs
@@ -7,35 +7,50 @@
 //oyun bitince tekrar oynamak istermisiniz diye sorsun
 //kullanıcı e/E girer ise oyun tekrar başlasın
 
-int sayi = new Random().Next(1, 100);
-int hak = 5; //5 hakkın var
-Console.WriteLine("Tutulan Sayi:" + sayi);
-Console.WriteLine("1-100 aralığında bir rasgele sayı tuttum.");
+char cevap;
 do
 {
-    Console.WriteLine("Tahminin:");
-    int tahmin = Convert.ToInt32(Console.ReadLine());
-    hak--;//hakkın bir azaldı
+    //Oyun kodları burada yer alacak
+    int sayi = new Random().Next(1, 100);
+    int hak = 5; //5 hakkın var
+    Console.WriteLine("Tutulan Sayi:" + sayi);
+    Console.WriteLine("1-100 aralığında bir rasgele sayı tuttum.");
+    do
+    {
+        Console.WriteLine("Tahminin:");
+        int tahmin = Convert.ToInt32(Console.ReadLine());
+        hak--;//hakkın bir azaldı
 
-    if(hak == 0 && tahmin != sayi)//hem hakkın bitti hem bilemedin
-    {
-        Console.WriteLine("Üzgünüm:) Bilemediniz.");
-        Console.WriteLine($"Tuttuğum sayı {sayi}");
-        break;
-    }
+        if (hak == 0 && tahmin != sayi)//hem hakkın bitti hem bilemedin
+        {
+            Console.WriteLine("Üzgünüm:) Bilemediniz.");
+            Console.WriteLine($"Tuttuğum sayı {sayi}");
+            break;
+        }
 
-    if(tahmin > sayi)
-    {
-        Console.WriteLine("Daha Küçük");
-    }
-    else if(tahmin < sayi)
-    {
-        Console.WriteLine("Daha Büyük");
-    }
-    else
-    {
-        Console.WriteLine("TEBRİKLER! BİLDİNİZ...");
-        break;//döngüden çıkar
-    }
+        if (tahmin > sayi)
+        {
+            Console.WriteLine("Daha Küçük");
+        }
+        else if (tahmin < sayi)
+        {
+            Console.WriteLine("Daha Büyük");
+        }
+        else
+        {
+            Console.WriteLine("TEBRİKLER! BİLDİNİZ...");
+            break;//döngüden çıkar
+        }
 
-} while(true);
+    } while (true);
+    //--------------------
+
+
+    Console.WriteLine("Tekrar oynamak ister misiniz?");
+    cevap = Convert.ToChar(Console.ReadLine());
+
+} while (cevap == 'E' || cevap == 'e');
+
+
+
+//uygulama sona erer
