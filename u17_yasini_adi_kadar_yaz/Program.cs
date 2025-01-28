4e7baf77c0cd0c825b11190e9ae8f9fec10c2540
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 04:34:18 +0300

    yeni örnekler

---
+++ u17_yasini_adi_kadar_yaz/Program.cs
@@ -0,0 +1,12 @@
+﻿//Klavyeden kişinin adı ve yaşı girilecek
+//Kişinin adını yaşı defa ekrana yazacak
+//for döngüsünü kullanın
+Console.WriteLine("Adınızı girin:");
+string ad = Console.ReadLine();
+Console.WriteLine("Yaşınızı girin:");
+int yas = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= yas; i++)
+{
+    Console.WriteLine($"{i} - {ad}");
+}
