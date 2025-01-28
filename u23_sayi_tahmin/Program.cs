ed38053719ab0eea522c465f7a1f9c7617859a2d
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 06:56:24 +0300

    oyun ile ilgili ödev

--- u23_sayi_tahmin/Program.cs
+++ u23_sayi_tahmin/Program.cs
@@ -1,20 +1,23 @@
 ﻿//+rasgele bir sayı tutabilme
 //+döngü içerinde kullanıcıdan tahmini alabilmeli
-//bilemdiğinde if ile kontrol edip yönlendirme yapmalı
-//hakkı bittiğinde döngüden çıkılmalı
-//bilemediğinde tuttuğu sayıyı söylesin
-//bilirse döngü sona ermeli ve tebrik etmeli
-int sayi = new Random().Next(100);
+//+bilemdiğinde if ile kontrol edip yönlendirme yapmalı
+//+hakkı bittiğinde döngüden çıkılmalı
+//+bilemediğinde tuttuğu sayıyı söylesin
+//+bilirse döngü sona ermeli ve tebrik etmeli
+//oyun bitince tekrar oynamak istermisiniz diye sorsun
+//kullanıcı e/E girer ise oyun tekrar başlasın
+
+int sayi = new Random().Next(1, 100);
 int hak = 5; //5 hakkın var
 Console.WriteLine("Tutulan Sayi:" + sayi);
-Console.WriteLine("0-100 aralığında bir rasgele sayı tuttum.");
+Console.WriteLine("1-100 aralığında bir rasgele sayı tuttum.");
 do
 {
     Console.WriteLine("Tahminin:");
     int tahmin = Convert.ToInt32(Console.ReadLine());
     hak--;//hakkın bir azaldı
 
-    if(hak == 0 && tahmin !=sayi)//hem hakkın bitti hem bilemedin
+    if(hak == 0 && tahmin != sayi)//hem hakkın bitti hem bilemedin
     {
         Console.WriteLine("Üzgünüm:) Bilemediniz.");
         Console.WriteLine($"Tuttuğum sayı {sayi}");
