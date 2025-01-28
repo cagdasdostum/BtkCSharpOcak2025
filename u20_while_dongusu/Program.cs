6f4940265f9d1f4d5e1735e4fb22d4db2d0f3862
Mustafa KURAL <mustafakural@outlook.com>
2025-01-28 06:03:16 +0300

    do while

---
+++ u20_while_dongusu/Program.cs
@@ -0,0 +1,17 @@
+﻿string pinKodu = "1234";
+string tahmin = "";
+int deneme = 0; //Daha hiç denemedin
+
+while(pinKodu != tahmin)//tahmin edilen doğru ise biter
+{
+    if(deneme > 0)//2.defa deniyorsan
+    {
+        Console.WriteLine("Bilemediniz. Tekrar deneyin.");
+    }
+
+    Console.WriteLine("Pin Kodunu Girin:");
+    tahmin = Console.ReadLine();
+    deneme++;//denediğin için 1 artır
+}
+
+Console.WriteLine("TEBRİKLER BİLDİNİZ :)");
---
+++ u21_do_while_dongusu/Program.cs
@@ -0,0 +1,14 @@
+﻿//klavyeden 0 girilene kadar girilmiş olan
+//sayıları toplayan örnek
+int sayi = 0;
+int toplam = 0; //hiç bir sayı girilmesiği için 0
+
+do {
+    Console.WriteLine("Bir sayı girin:");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    toplam += sayi;
+
+} while(sayi!=0);
+
+Console.WriteLine("Toplam = " + toplam);
+
