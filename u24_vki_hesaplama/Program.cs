e65ac492841a7b46191e920ec9ff067cd7e5e35e
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 00:46:39 +0300

    vüvut kitle indeksi

---
+++ u24_vki_hesaplama/Program.cs
@@ -0,0 +1,37 @@
+﻿//Klavyeden kilo ve boy bilgisi alınacak
+//vücut kitle indeksi hesapalnacak
+//vki = kg / m2  ---> kilo / boy karesi
+//<18.5 ---> Zayıf
+//>18.5-25.0 ----> Normal
+//>25.0-30 -----> Kilolu
+//>30 -----> Aşırı Kilolu
+
+Console.WriteLine("Kilonuzu girin(kg):");
+double kilo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Boyunuzu girin(cm):");
+double boy = Convert.ToDouble(Console.ReadLine());
+
+//boy/=100; //bileşik atama operatörü boy = boy / 100
+
+double boyMetre = boy / 100; //cm ---> metre dönüştür
+double vki = kilo / (boyMetre * boyMetre);
+
+Console.WriteLine($"Vücut Kitle İndeksi = {vki:f2}");
+
+if(vki < 18.5)
+{
+     Console.WriteLine("Zayıf");
+}
+else if(vki >=18.5 && vki < 25)
+{
+    Console.WriteLine("Normal");
+}
+else if(vki >=25 && vki < 30)
+{
+    Console.WriteLine("Kilolu");
+}
+else //30 ve üzeri ise
+{
+    Console.WriteLine("Aşırı Kilolu");
+}
