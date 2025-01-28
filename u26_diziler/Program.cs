582e231c35f1a3b2b25b33b79de7208a9ccf11be
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 01:43:37 +0300

    dizi tanımlandı ekrana elemenler yazdırıldı

---
+++ u26_diziler/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] sayilar = [4, 8, 2, 1, -3];
+
+//int s1 = 4
+//int s2 = 8 ......
+
+Console.WriteLine("Sayı Adedi:" + sayilar.Length);
+Console.WriteLine("Dizideki Sayılar:");
+
+for(int i = 0; i < sayilar.Length; i++)
+{
+    Console.Write($"{sayilar[i]}, ");
+}
