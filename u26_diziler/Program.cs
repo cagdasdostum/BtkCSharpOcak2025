2b9359eb47a1a04284daaadefa0be3ebc2fe0ff8
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 02:04:01 +0300

    öğrenci ortalama dizisi

--- u26_diziler/Program.cs
+++ u26_diziler/Program.cs
@@ -1,8 +1,6 @@
 ﻿int[] sayilar = [4, 8, 2, 1, -3];
-
 //int s1 = 4
 //int s2 = 8 ......
-
 Console.WriteLine("Sayı Adedi:" + sayilar.Length);
 Console.WriteLine("Dizideki Sayılar:");
 
@@ -10,3 +8,12 @@ for(int i = 0; i < sayilar.Length; i++)
 {
     Console.Write($"{sayilar[i]}, ");
 }
+
+int toplam = 0;
+
+foreach(int sayi in sayilar) // for(int i=0;i<sayilar.Length;i++)
+{                            // sayi = sayilar[i]
+    toplam += sayi;
+}
+
+Console.WriteLine($"\nDizideki Sayıların Toplamı:{toplam}");
---
+++ u27_ogrenci_ort_dizisi/Program.cs
@@ -0,0 +1,24 @@
+﻿//1.Öğrencinin Adını Gir: Mustafa
+//1.Öğrencinin Ortalamasını Gir: 70
+//2.Öğrencinin Adını Gir: Ali
+//2.Öğrencinin Ortalamasını Gir: 80
+
+//Öğrenci Adı         | Ortalama
+//-------------------   --------
+//Mustafa                     70
+//Ali                         80
+
+//Sınıf Ortalaması : 75
+//En Başarılı Öğrenci: Ali 
+string[] isimler = new string[3];
+double[] ortalamalar = new double[3];
+
+//Verileri Oku ve Dizilere yerleştir
+for(int i = 0;i < isimler.Length; i++)
+{
+    Console.WriteLine($"{i + 1}.Öğrencinin Adını Gir:");
+    isimler[i] = Console.ReadLine();
+    Console.WriteLine($"{i + 1}.Öğrencinin Ortalamasını Gir:");
+    ortalamalar[i] = Convert.ToDouble(Console.ReadLine());
+}
+
