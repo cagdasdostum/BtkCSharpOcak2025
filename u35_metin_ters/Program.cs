15ba4aec088be42a7c5d581958c758ae53ef85bc
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 07:10:03 +0300

    metni stach ile tersten yazdır

---
+++ u35_metin_ters/Program.cs
@@ -0,0 +1,21 @@
+﻿//klavyeden girilen bir metni
+//stack kullanarak tersinden yazdırın
+//Örnek;
+//Bir metin girin: Programlama
+//amalmargorP
+Console.WriteLine("Bir metin girin:");
+string metin = Console.ReadLine();
+
+Stack<char> yigin = new ();
+//metindeki tüm karakterleri döngü ile dolaş
+//ve yigin içerisine ekle
+foreach (char c in metin) 
+{
+    yigin.Push(c);
+}
+//yığındaki her karakteri yanyana ekrana yazdır
+while (yigin.Count > 0)
+{
+    char c = yigin.Pop();
+    Console.Write(c);
+}
