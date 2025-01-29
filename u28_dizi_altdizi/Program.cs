27ce7f54a0e55fc33428bd21806f7b628a56bec6
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 04:14:52 +0300

    düzeltme

--- u28_dizi_altdizi/Program.cs
+++ u28_dizi_altdizi/Program.cs
@@ -27,8 +27,9 @@ for(int i = 0;i < yeniDizi2.Length;i++) //foreach ile aynı mantık
 //Eskiden
 Console.WriteLine(sayilar[sayilar.Length - 1]);//sayilar[^1]
 int bas=2, bit=6;
-var yeniDizi3 = new int[6-2];//yeni dizi 4 eleman
+var yeniDizi3 = new int[bit-bas];//yeni dizi 4 eleman
 for(int i = bas; i <= bit; i++) //sayilar[2..6]
 {
     yeniDizi3[i-bas] = sayilar[i];
 }
+
