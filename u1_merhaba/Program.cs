a9765823d732977b02a165c6bd5ee6460fe26216
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 04:06:03 +0300

    dizi alt dizi

--- u1_merhaba/Program.cs
+++ u1_merhaba/Program.cs
@@ -1,8 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("BTK AKADEMİ!");//metin
 Console.WriteLine("İzmir 2025");
 Console.WriteLine(2025);//tamsayı
 Console.WriteLine(4.5);//ondalıklı sayı
 Console.WriteLine(true);//mantıksal
 Console.WriteLine('D');//karakter
-
---
+++ u28_dizi_altdizi/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] sayilar = [5, 1, 0, 9, 12, 4, 8];
+
+Console.WriteLine(sayilar[2]);//0
+Console.WriteLine(sayilar[^3]);//12
+var yeniDizi = sayilar[2..5];//0,9,12
+foreach(var s in yeniDizi)
+{
+    Console.Write($"{s}, ");
+}
+
+Console.WriteLine();//boş yani alta geç
+
+var yeniDizi2 = sayilar[^6..];
+foreach(var s in yeniDizi2)
+{
+    Console.Write($"{s}, ");
+}
