b0cbdd377859c9e55a18d5ec7b4e08c805bb93be
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 05:58:01 +0300

    arraylist ve list

---
+++ u32_arraylist_ve_list/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections;
+
+ArrayList arrayList = ["İzmir", 35, 3.14, true];
+arrayList.Add("Btk");
+arrayList.Insert(2, -1);
+foreach(var i in arrayList)
+{
+    Console.Write($"{i}, ");
+}
