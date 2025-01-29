4ff18cc36b32606c0ea7b3c77d9b9a123a10c01c
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 05:16:26 +0300

    parola uygun değilse dön

--- u31_parola_uygunluk_test/Program.cs
+++ u31_parola_uygunluk_test/Program.cs
@@ -1,37 +1,44 @@
-﻿Console.WriteLine("Yeni Parola Girin:");
-string parola = Console.ReadLine();
-//+parola en az 6 karakter olmalı
-//parolada harfler olmalı
-//rakamlar olmalı
-//özel karakterler olmalı
-//Örnek; abcd1234?
-int harfler = 0, rakamlar = 0, ozelKar = 0;
+﻿bool uygun = false;//************
 
-if(parola.Length>=6)
+do
 {
-    foreach(char c in parola)
-    {
-        if(char.IsLetter(c))
-            harfler++;
-        else if(char.IsDigit(c))
-            rakamlar++;
-        else if(char.IsSymbol(c))
-            ozelKar++;
-    }
 
-    if(rakamlar > 0 && harfler > 0 && ozelKar > 0)
+    Console.WriteLine("Yeni Parola Girin:");
+    string parola = Console.ReadLine();
+    //+parola en az 6 karakter olmalı
+    //parolada harfler olmalı
+    //rakamlar olmalı
+    //özel karakterler olmalı
+    //Örnek; abcd1234?
+    int harfler = 0, rakamlar = 0, ozelKar = 0;
+
+
+    if (parola.Length >= 6)
     {
-        Console.WriteLine("Parola Uygun...");
+        foreach (char c in parola)
+        {
+            if (char.IsLetter(c))
+                harfler++;
+            else if (char.IsDigit(c))
+                rakamlar++;
+            else if (char.IsSymbol(c) || char.IsPunctuation(c))
+                ozelKar++;
+        }
+
+        if (rakamlar > 0 && harfler > 0 && ozelKar > 0)
+        {
+            Console.WriteLine("Parola Uygun...");
+            uygun = true;//*****************
+        }
+        else
+        {
+            Console.WriteLine("Parolanızın içinde harf, rakam ve " +
+                                "özel karakterler olmalı!");
+        }
     }
     else
     {
-        Console.WriteLine("Parolanızın içinde harf, rakam ve" +
-                            "özel karakterler olmalı!");
+        Console.WriteLine("Parolanız en az 6 karakter olmalı.");
     }
-}
-else 
-{
-    Console.WriteLine("Parolanız en az 6 karakter olmalı.");
-}
 
-
+} while (!uygun);//******Uygun değilse tekrarla
