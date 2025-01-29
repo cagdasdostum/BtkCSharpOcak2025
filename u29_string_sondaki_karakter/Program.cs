2c3c3799eae1653876aaae6f81759db9c011e522
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 04:25:35 +0300

    string de bir dizidir

---
+++ u29_string_sondaki_karakter/Program.cs
@@ -0,0 +1,14 @@
+﻿string metin = "Btk akademi ile C# Programlama Eğitimi";
+char[] metin2 = { 'B', 't', 'k', ' ', 'a', 'k', 'a', 'd', 'e', 'm', 'i'};
+//string - Karakter Dizesi (dizisi)
+//string özelleştirilmiş bir dizidir
+
+Console.WriteLine(metin[0]);
+Console.WriteLine(metin[^1]);  
+Console.WriteLine(metin[4..11]); 
+Console.WriteLine(metin2);
+
+foreach(var c in metin)
+{
+    Console.Write($"{c}-");
+}
