ec18833e793fd04469ca7b17e97617fb26b9bd3c
Mustafa KURAL <mustafakural@outlook.com>
2025-01-29 04:13:49 +0300

    eskiden alt dizi oluşturmak

--- u28_dizi_altdizi/Program.cs
+++ u28_dizi_altdizi/Program.cs
@@ -23,3 +23,12 @@ for(int i = 0;i < yeniDizi2.Length;i++) //foreach ile aynı mantık
     var s = yeniDizi2[i];
      Console.Write($"{s}, ");
 }
+
+//Eskiden
+Console.WriteLine(sayilar[sayilar.Length - 1]);//sayilar[^1]
+int bas=2, bit=6;
+var yeniDizi3 = new int[6-2];//yeni dizi 4 eleman
+for(int i = bas; i <= bit; i++) //sayilar[2..6]
+{
+    yeniDizi3[i-bas] = sayilar[i];
+}
