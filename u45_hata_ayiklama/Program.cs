60a8039c42a3377ab4b5c26e01ec2b90ffc63485
Mustafa KURAL <mustafakural@outlook.com>
2025-01-31 00:38:47 +0300

    hata yakalama

---
+++ u45_hata_ayiklama/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hesaplama Uygulaması");
+
+
+int s1 = 0, s2 = 0;//uygulama için global
+
+try
+{
+    //bu aralıkta hata oluşursa
+    Console.WriteLine("1.Sayıyı gir");
+    s1 = Convert.ToInt32(Console.ReadLine());
+    //Convert.ToInt32("beş")--hata
+    Console.WriteLine("2.Sayıyı gir");
+    s2 = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    //burada hatayı yakala ve işle
+    Console.WriteLine("Hata oluştu.");
+    return;//main metodunu sonlandırır
+}
+
+
+
+int top = s1 + s2;
+
+Console.WriteLine($"Toplam = {top}");
