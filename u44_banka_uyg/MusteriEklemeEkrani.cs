2369dd02d49865f53fe1ae4587fd8e7fc06789d1
Mustafa KURAL <mustafakural@outlook.com>
2025-01-30 06:59:55 +0300

    müşteri ekleme ekranı

---
+++ u44_banka_uyg/MusteriEklemeEkrani.cs
@@ -0,0 +1,20 @@
+﻿namespace u44_banka_uyg;
+
+public class MusteriEklemeEkrani
+{
+    public static void Goster(List<Musteri> liste)
+    {
+        Console.Clear();
+        Console.WriteLine("Eklemek İstediğiniz Müşteri Adı Soyadı:");
+        string adSoyad = Console.ReadLine();
+
+        Musteri musteri = new ();
+        musteri.AdSoyad = adSoyad;
+        musteri.MusteriNo = (int)DateTime.Now.Ticks;//o anki zaman 
+
+        liste.Add(musteri);
+        
+        Console.WriteLine("Müşteri eklendi. Devam etmek için bir tuşa bas...");
+        Console.ReadKey();//bir tuşa basılana kadar bekle
+    } 
+}
