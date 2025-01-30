5a28638f62bb320e6369b0824f2a1fed7257c4f1
Mustafa KURAL <mustafakural@outlook.com>
2025-01-30 07:08:23 +0300

    Müşteri listeleme ekranı

---
+++ u44_banka_uyg/MusteriListemeEkrani.cs
@@ -0,0 +1,15 @@
+﻿namespace u44_banka_uyg;
+
+public class MusteriListemeEkrani
+{
+    public static void Goster(List<Musteri> liste)
+    {
+        Console.Clear();
+        Console.WriteLine("Müşteri Listesi");
+
+        foreach (Musteri m in liste)
+        {
+            m.Yazdir();
+        }
+    }
+}
