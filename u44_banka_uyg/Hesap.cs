3a06ab7582a4bde9d1150628c1263feb7a3692ab
Mustafa KURAL <mustafakural@outlook.com>
2025-01-30 06:51:01 +0300

    hesap yazdir

--- u44_banka_uyg/Hesap.cs
+++ u44_banka_uyg/Hesap.cs
@@ -5,4 +5,10 @@ public class Hesap
     public int HesapNumarasi { get; set; }
     public string HesapTuru { get; set; }   
     public double Bakiye { get; set; }
+
+    public void Yazdir()
+    {
+        Console.WriteLine($"Hesap Numarası: {HesapNumarasi} "
+        + $"- Hesap Türü: {HesapTuru} - Bakiye: {Bakiye}");
+    }
 }
