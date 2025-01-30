24ed845b44798b39218d7486059d6666549ff347
Mustafa KURAL <mustafakural@outlook.com>
2025-01-30 04:20:33 +0300

    rasgele 6 sayı üret

---
+++ u40_rasgele_6sayi/Program.cs
@@ -0,0 +1,31 @@
+﻿//rasgele 1-49 arasında 6 adet sayı üreten ve
+//ürettiği sayıları bir dizi şeklinde dönen
+//bir metot yazınız
+
+//int[] sayilar = RasgeleUret();
+
+int[] RasgeleUret()
+{
+    List<int> sonuc = new();
+    Random rnd = new ();
+
+    while(sonuc.Count < 6)
+    {
+        int sayi = rnd.Next(1, 49 + 1);
+        sonuc.Add(sayi);
+    }
+
+    return sonuc.ToArray();//listeyi diziye dönüştür
+}
+
+void Yazdir(int[] liste)
+{
+    foreach (int s in liste)
+    {
+        Console.Write($"{s}, ");
+    }
+    Console.WriteLine();
+}
+///ANA PROGRAM----------------------
+int[] sayilar = RasgeleUret();
+Yazdir(sayilar);
