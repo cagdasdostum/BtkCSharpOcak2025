c0990c90238627115270c7c42e65ca7e0bbb95b1
Mustafa KURAL <mustafakural@outlook.com>
2025-01-31 04:40:01 +0300

    anaprogram döngüsü yazıldı

--- u49_btk_console/Program.cs
+++ u49_btk_console/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+using u49_btk_console;
+
+do
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(">");
+    string komut = Console.ReadLine();//cp yol1 yol2
+    string[] komutArg = komut.Split(' ');
+
+    switch (komutArg[0])
+    {
+        case "help": KomutHelp.Calistir();break;
+        default: 
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Komut bulunanmadı."); 
+        break;
+    }
+
+} while(true);
