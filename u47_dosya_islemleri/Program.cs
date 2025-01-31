381b45554f312cc1d23e1882c2c773e974c185cf
Mustafa KURAL <mustafakural@outlook.com>
2025-01-31 03:39:17 +0300

    dosya oluşturma işlemleri

---
+++ u47_dosya_islemleri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+string folderPath = "D:\\BtkAkademiOcak2025";
+if(!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+    Console.WriteLine("Klasör oluşturuldu.");
+}
+
+string filePath = folderPath + "\\merhaba.txt";
+string icerik = "Merhaba, c# programlama dili.";
+
+File.WriteAllText(filePath, icerik);
+Console.WriteLine("Dosya oluşturuldu.");
+
