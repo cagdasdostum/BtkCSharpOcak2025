5bab7ceb862250f80cc3bd223a0dd013994621f6
Mustafa KURAL <mustafakural@outlook.com>
2025-01-31 05:26:00 +0300

    cd komutu

---
+++ u49_btk_console/KomutCd.cs
@@ -0,0 +1,21 @@
+﻿namespace u49_btk_console;
+
+public class KomutCd
+{
+    public static void Calistir(string path, string[] args)
+    {
+        if(args.Length<2)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Komut parametreleri eksik yada geçersiz.");
+            return;
+        }
+
+        string hedef = args[1]; //hedef klasör yada ..
+
+        if(hedef == "..")
+        {
+            string hedefPath = Path.GetDirectoryName(path);
+        }
+    }
+}
