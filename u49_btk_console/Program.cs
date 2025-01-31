de3fb869635fcf4f176ee5fa1a0d8a1af758a633
Mustafa KURAL <mustafakural@outlook.com>
2025-01-31 04:56:40 +0300

    ls komutu eklendi

--- u49_btk_console/Program.cs
+++ u49_btk_console/Program.cs
@@ -1,16 +1,23 @@
 ﻿
 using u49_btk_console;
 
+string path;
+
 do
 {
     Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine(">");
+    path = Directory.GetCurrentDirectory();
+
+    Console.Write($"BTK {Path.GetFileName(path)}>");
+
     string komut = Console.ReadLine();//cp yol1 yol2
     string[] komutArg = komut.Split(' ');
 
     switch (komutArg[0])
     {
         case "help": KomutHelp.Calistir();break;
+        case "exit": return;//ana metodu sonlandır
+            
         default: 
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Komut bulunanmadı."); 
