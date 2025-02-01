af4f7db99171b838d86e75137648c1ee63d4dc38
Mustafa KURAL <mustafakural@outlook.com>
2025-02-01 04:58:07 +0300

    kişi güncelleme işlemi yapıldı

---
+++ u51_ef_telrehberi/GuncelleEkrani.cs
@@ -0,0 +1,38 @@
+﻿namespace u51_ef_telrehberi;
+
+public class GuncelleEkrani
+{
+ public static void Goster(Db baglanti)
+    {
+        Console.WriteLine("Güncelleme Ekranı");//****
+        //READ--------------
+        Console.WriteLine("Güncellenecek kişinin Id:");//****
+        int id = Convert.ToInt32(Console.ReadLine());//****
+        Kisi kisi = baglanti.Kisiler.Find(id);//*****
+        //Kisi kisi = new();
+        if(kisi == null)//****
+        {
+            Console.WriteLine("Kişi bulunamadı!");//****
+            return;//****
+        }
+
+        Console.Write("Adı girin:");
+        kisi.Ad = Console.ReadLine();
+        
+        Console.Write("Soyadı girin:");
+        kisi.Soyad = Console.ReadLine();
+
+        Console.Write("Tel girin:");
+        kisi.Tel = Console.ReadLine();
+        
+        Console.Write("Adres girin:");
+        kisi.Adres = Console.ReadLine();
+        //CREATE-------------------------
+        baglanti.Kisiler.Add(kisi);
+        baglanti.SaveChanges();
+        //-------------------------------
+        Console.WriteLine("Kişi güncellenedi edildi.");//*****
+        Console.WriteLine("Devam etmek için bir tuşa bas...");
+        Console.ReadKey();
+    }
+}
